d124a88126f070a85ca9f2dda7ecd0cde6eacdc7
Kishan Kumar <Kishan.javatrainer@gmail.com>
2018-04-23 10:41:13 +0530

    code

---
+++ SerializationExample_5/src/com/infotech/model/Employee.java
@@ -0,0 +1,58 @@
+package com.infotech.model;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Employee extends Person implements Externalizable {
+
+	private static final long serialVersionUID = -4724957588046435474L;
+	
+	public Employee() {
+		System.out.println("Employee default constructor is called..");
+	}
+	
+	private String email;
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		//Writing parent class ClassA fields
+		out.writeInt(getId());
+		out.writeObject(getName());
+		out.writeInt(getAge());
+		
+		// Writing child class fields
+		out.writeObject(getEmail());
+		out.writeObject(getPassword());
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		// Setting parent class fields
+			setId(in.readInt());
+			setName((String)in.readObject());
+			setAge(in.readInt());
+
+			// Setting child class fields
+			setEmail((String)in.readObject());
+			setPassword((String)in.readObject());
+	}
+}
